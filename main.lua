984dd2c44e029ddbccb932a4f5170e7b3f3ab0db
Nugz69 <69019699+Nugz69@users.noreply.github.com>
2022-04-11 18:13:34 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,19 @@
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+
+local GUI = Mercury:Create{
+    Name = "NugzyCMD",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://github.com/deeeity/mercury-lib"
+}
+local celf = GUI:Tab{
+	Name = "Self",
+	Icon = "rbxassetid://8569322835"
+}
+
+celf:button({
+
+    Name = "MODIFY"
+    
+
+})
