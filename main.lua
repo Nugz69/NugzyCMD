178ca500fa9f46dad23c6f07e82c5276e4f7de71
Nugz69 <69019699+Nugz69@users.noreply.github.com>
2022-04-11 18:41:32 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,6 @@
 local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
+local plyr = game:GetService("Players").LocalPlayer
+
 
 local GUI = Mercury:Create{
     Name = "NugzyCMD",
@@ -11,21 +13,41 @@ local celf = GUI:Tab{
 	Icon = "rbxassetid://8569322835"
 }
 
-celf:button({
+celf:Toggle({
+    
+    Name = "GodMode",
+    StartingState = false,
+    Description = "Makes yourself invincible",
+    Callback = function(bool)
 
-    Name = "Modify",
-    Callback = function()
-        celf:prompt{
+        if bool then
+            
+            godMode(plyr);
+            celf:Notification{
 
-            Title = "GodMode",
-            Text = "Make yourself invincible",
-            Buttons = {
+                Title = "ALERT" , 
+                Text = "GodMode has been enabled",
+                Duration = 3;
+                Callback = function()end
 
-                Ok = function()
-
-                end
             }
-        
-        }
+
+        end
+
     end
+
 })
+
+function godMode () 
+
+    if plyr.Character then 
+
+        for i , v in pairs (plyr.Character:GetChildren()) do 
+            if v.Name == "hitbox" then 
+                v:Destroy()
+            end
+        end
+    end
+
+end
+
