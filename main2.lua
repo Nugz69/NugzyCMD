a7a8d2344dc1a685b69c2aa1706f7b356db860a7
Nugz69 <69019699+Nugz69@users.noreply.github.com>
2022-04-11 19:25:30 -0500

    Update main2.lua

--- main2.lua
+++ main2.lua
@@ -2,35 +2,54 @@ local VLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/vep1032/
 
 local s = VLib:Window("VEP HUB", "GAMENAME", "G")
 
-local ss = s:Tab("Tab 1")
+local ss = s:Tab("Self")
 
-ss:Button("Button",function()
-print("Pressed!")
+ss:Toggle("GodMode",function(t)
+
+    local LocalPlayer  = game:getService("Players").LocalPlayer
+    print (t)
+    if t then
+        if LocalPlayer.Character then
+            for i , v in pairs (plyr.Character:GetChildren()) do 
+                if v.Name == "hitbox" then 
+                    v:Destroy()
+                end
+            end 
+        else
+            t = false
+        end  
+    end 
+    
+
 end)
 
-
-ss:Toggle("Toggle",function(t)
-print(t)
-    end)
-
-
-ss:Slider("Slider",0,100,70,function(t)
-print(t)
-end)
+--ss:Button("Button",function()
+--print("Pressed!")
+--end)
 
 
-ss:Dropdown("Dropdown",{"Option 1", "Option 2", "Option 3", "Option 4", "Option 5"},function(t)
-print(t)
-end)
+--ss:Toggle("Toggle",function(t)
+--print(t)
+   -- end)
+
+
+---ss:Slider("Slider",0,100,70,function(t)
+--print(t)
+--end)
+
+
+--ss:Dropdown("Dropdown",{"Option 1", "Option 2", "Option 3", "Option 4", "Option 5"},function(t)
+--print(t)
+--end)
    
    
-ss:Colorpicker("Colorpicker",Color3.fromRGB(255, 1, 1),function(t)
-print(t)
-    end)
+--ss:Colorpicker("Colorpicker",Color3.fromRGB(255, 1, 1),function(t)
+--print(t)
+    --end)
 
-ss:Textbox("Textbox", true,function(t)
-print(t)
-end)
+--ss:Textbox("Textbox", true,function(t)
+--print(t)
+--end)
 
-ss:Label("Label")
-s:Tab("Tab 2")
+--ss:Label("Label")
+--s:Tab("Tab 2")
