5c46b7440c964f4cf63d0b844b766e4ebc69b6cd
Nugz69 <69019699+Nugz69@users.noreply.github.com>
2022-04-11 20:32:44 -0500

    Update main2.lua

--- main2.lua
+++ main2.lua
@@ -23,23 +23,26 @@ ss:Toggle("GodMode",function(t)
     
 
 end)
-ss:Toggle("Fly" , function(t)
+--ss:Toggle("Fly" , function(t)
 
-    print(t)
-    local plyr = game:getService("Players").LocalPlayer
-    local _2r2t = Instance.new("ScreenGui") local MainGUI = Instance.new("Frame") local title = Instance.new("TextLabel") local speed = Instance.new("TextButton") local noclip = Instance.new("TextButton") local close = Instance.new("TextButton") local OpenGUI = Instance.new("Frame") local open = Instance.new("TextButton") --Properties: 
-    _2r2t.Name = "2r2t" _2r2t.Parent = game.CoreGui  MainGUI.Name = "MainGUI" MainGUI.Parent = _2r2t MainGUI.BackgroundColor3 = Color3.new(0.156863, 0.156863, 0.156863) MainGUI.BackgroundTransparency = 0.15000000596046 MainGUI.BorderColor3 = Color3.new(0.666667, 0, 0) MainGUI.BorderSizePixel = 5 MainGUI.Position = UDim2.new(0.121778354, 0, 0.364864916, 0) MainGUI.Size = UDim2.new(0, 193, 0, 143) MainGUI.Visible = false MainGUI.Active = true MainGUI.Draggable = true  title.Name = "title" title.Parent = MainGUI title.BackgroundColor3 = Color3.new(0.156863, 0.156863, 0.156863) title.BorderColor3 = Color3.new(0)
+  --  print(t)
+    --local plyr = game:getService("Players").LocalPlayer
+    --local _2r2t = Instance.new("ScreenGui") local MainGUI = Instance.new("Frame") local title = Instance.new("TextLabel") local speed = Instance.new("TextButton") local noclip = Instance.new("TextButton") local close = Instance.new("TextButton") local OpenGUI = Instance.new("Frame") local open = Instance.new("TextButton") --Properties: 
+    --_2r2t.Name = "2r2t" _2r2t.Parent = game.CoreGui  MainGUI.Name = "MainGUI" MainGUI.Parent = _2r2t MainGUI.BackgroundColor3 = Color3.new(0.156863, 0.156863, 0.156863) MainGUI.BackgroundTransparency = 0.15000000596046 MainGUI.BorderColor3 = Color3.new(0.666667, 0, 0) MainGUI.BorderSizePixel = 5 MainGUI.Position = UDim2.new(0.121778354, 0, 0.364864916, 0) MainGUI.Size = UDim2.new(0, 193, 0, 143) MainGUI.Visible = false MainGUI.Active = true MainGUI.Draggable = true  title.Name = "title" title.Parent = MainGUI title.BackgroundColor3 = Color3.new(0.156863, 0.156863, 0.156863) title.BorderColor3 = Color3.new(0)
     
 
-end)
+--end)
 
-ss:Slider("WalkSpeed",0,100,70,function(t) 
+ss:Slider("WalkSpeed",0,100,0,function(t) 
 
     local plr  = game.Players.LocalPlayer
+    local char = player.CharacterAdded:wait()
+    local h = char.Humanoid
 
     if plr.Character then
-    
-        Character.Humanoid.WalkSpeed = t 
+        
+        local a  = t 
+        h.WalkSpeed = a 
         print(t)
 
     end 
