697107277898a786f315cdfd5beebfec326d3b35
Nugz69 <69019699+Nugz69@users.noreply.github.com>
2022-04-11 19:33:25 -0500

    Update main2.lua

--- main2.lua
+++ main2.lua
@@ -1,8 +1,9 @@
 local VLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/vep1032/VepStuff/main/VL"))()
 
-local s = VLib:Window("VEP HUB", "GAMENAME", "G")
+local s = VLib:Window("NugzyCMD", "----------", "")
 
 local ss = s:Tab("Self")
+
 
 ss:Toggle("GodMode",function(t)
 
@@ -22,6 +23,119 @@ ss:Toggle("GodMode",function(t)
     
 
 end)
+ss:Toggle("Fly",function(t)
+
+    local function NQWSTGE_fake_script() -- Frame.Fly 
+        local script = Instance.new('LocalScript', Frame)
+     
+        local plr = script.Parent.Parent.Parent.Parent
+        repeat wait() until plr and plr.Character and plr.Character:findFirstChild("HumanoidRootPart") and plr.Character:findFirstChild("Humanoid") 
+        local mouse = game.Players.LocalPlayer:GetMouse()  
+        repeat wait() until mouse
+        
+        local torso = plr.Character.HumanoidRootPart 
+        local flying = false
+        local deb = true 
+        local ctrl = {f = 0, b = 0, l = 0, r = 0} 
+        local lastctrl = {f = 0, b = 0, l = 0, r = 0} 
+        local maxspeed = 1000 
+        local speed = 50
+        function Fly() 
+        local bg = Instance.new("BodyGyro", torso) 
+        bg.P = 9e4 
+        bg.maxTorque = Vector3.new(9e9, 9e9, 9e9) 
+        bg.cframe = torso.CFrame 
+        local bv = Instance.new("BodyVelocity", torso) 
+        bv.velocity = Vector3.new(0,0.1,0) 
+        bv.maxForce = Vector3.new(9e9, 9e9, 9e9) 
+        repeat wait() 
+        plr.Character.Humanoid.PlatformStand = true 
+        if ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0 then 
+        speed = speed+.5+(speed/maxspeed) 
+        if speed > maxspeed then 
+        speed = maxspeed 
+        end 
+        elseif not (ctrl.l + ctrl.r ~= 0 or ctrl.f + ctrl.b ~= 0) and speed ~= 0 then 
+        speed = speed-1 
+        if speed < 0 then 
+                        speed = 0
+                    else
+                        speed = 50
+        end 
+        end 
+        if (ctrl.l + ctrl.r) ~= 0 or (ctrl.f + ctrl.b) ~= 0 then 
+        bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (ctrl.f+ctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(ctrl.l+ctrl.r,(ctrl.f+ctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
+        lastctrl = {f = ctrl.f, b = ctrl.b, l = ctrl.l, r = ctrl.r} 
+        elseif (ctrl.l + ctrl.r) == 0 and (ctrl.f + ctrl.b) == 0 and speed ~= 0 then 
+        bv.velocity = ((game.Workspace.CurrentCamera.CoordinateFrame.lookVector * (lastctrl.f+lastctrl.b)) + ((game.Workspace.CurrentCamera.CoordinateFrame * CFrame.new(lastctrl.l+lastctrl.r,(lastctrl.f+lastctrl.b)*.2,0).p) - game.Workspace.CurrentCamera.CoordinateFrame.p))*speed 
+        else 
+        bv.velocity = Vector3.new(0,0.1,0) 
+        end 
+        bg.cframe = game.Workspace.CurrentCamera.CoordinateFrame * CFrame.Angles(-math.rad((ctrl.f+ctrl.b)*50*speed/maxspeed),0,0) 
+        until not flying 
+        ctrl = {f = 0, b = 0, l = 0, r = 0} 
+        lastctrl = {f = 0, b = 0, l = 0, r = 0} 
+            
+        bg:Destroy() 
+        bv:Destroy() 
+            plr.Character.Humanoid.PlatformStand = false 
+            speed = 50
+        end 
+        
+        mouse.KeyDown:connect(function(key) 
+        if key:lower() == "e" then 
+                if flying then flying = false 
+                    speed = 50
+        else 
+        flying = true 
+        Fly()
+        
+        end 
+        elseif key:lower() == "w" then 
+        ctrl.f = 1 
+        elseif key:lower() == "s" then 
+        ctrl.b = -1 
+        elseif key:lower() == "a" then 
+        ctrl.l = -1 
+        elseif key:lower() == "d" then 
+        ctrl.r = 1 
+        end 
+        end) 
+        mouse.KeyUp:connect(function(key) 
+        if key:lower() == "w" then 
+        ctrl.f = 0 
+        elseif key:lower() == "s" then 
+        ctrl.b = 0 
+        elseif key:lower() == "a" then 
+        ctrl.l = 0 
+        elseif key:lower() == "d" then 
+        ctrl.r = 0 
+        end 
+        end)
+        
+        plr.Character.Humanoid.StateChanged:Connect(function(o,n)
+            if n == Enum.HumanoidStateType.Running then
+                ctrl.f = 1
+            else
+                ctrl.f = 0
+            end
+        
+        end)
+        script.Parent.TextButton.MouseButton1Click:Connect(function()
+            if flying then
+                flying = false
+                speed = 50
+            else
+                flying = true
+                Fly()
+            end
+        end)
+        
+    end
+    coroutine.wrap(NQWSTGE_fake_script)()
+
+end)
+
 
 --ss:Button("Button",function()
 --print("Pressed!")
